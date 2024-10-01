5a068efa88399a15a670a80f11a9973ea5bdd38a
George Rodgers <george.rodgers.chem@gmail.com>
2024-10-01 12:54:08 +0100

    Post workshop update

--- classes/Movie.js
+++ classes/Movie.js
@@ -1,6 +1,26 @@
 // import the Media class:
 
+const Media = require(`./Media`);
+
 // create your Movie class:
+
+class Movie extends Media{
+    constructor(title, year, genre, director, duration, rating){
+        super(title, year, genre);
+        this.director = director;
+        this.duration = duration;
+        this.rating = rating;
+    }
+    summary(){
+        return `Title: ${this.title}, Director: ${this.director}, Year: ${this.year}, Genre: ${this.genre}, Duration: ${this.genre}, Rating: ${this.rating}`
+    }
+    static longestMovie(movie){
+        for (let i = 0; i < movie.length; i++){
+            if (movie[i].duration === Math.max(movie.duration))
+                return movie[i];
+    }
+}
+}
 
 // don't change below
 module.exports = Movie;
--- classes/Music.js
+++ classes/Music.js
@@ -1,6 +1,26 @@
 // import the Media class:
 
+const Media = require(`./Media`);
+
 // create your Music class:
+
+class Music extends Media{
+    constructor(title, year, genre, artist, length){
+        super(title, year, genre);
+        this.artist = artist;
+        this.length = length;
+    }
+
+    summary(){
+        return `Title: ${this.title}, Artist: ${this.artist}, Year: ${this.year}, Genre: ${this.genre}, Length: ${this.length} seconds`
+    }
+    static shortestAlbum(albums){
+        for (let i = 0; i < albums.length; i++){
+            if (albums[i].length === Math.min(albums.length))
+                return albums[i];
+    }
+}
+}
 
 // don't change below
 module.exports = Music;
